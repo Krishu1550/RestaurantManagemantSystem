93d9ed70163fc2ee2bf19683dd5b0fe091dd833a
SHAILESH KUMAR <75526119+Krishu1550@users.noreply.github.com>
2023-03-31 17:40:17 +0200

    Add files via upload

---
+++ RestaurantManagementSystem.BusinessLogicLayer/RestaurantMgmtBLL.cs
@@ -0,0 +1,42 @@
+﻿using RestaurantManagementSystem.DomainEntities;
+using System.Data;
+
+using RestaurantManagementSystem.DataAccessLayer;
+namespace RestaurantManagementSystem.BusinessLogicLayer
+{
+    public class RestaurantMgmtBLL
+    {
+        
+
+        public static bool CreateRestaurant(Restaurant restaurant)
+        {
+            int AffectedRow = RestaurantMgmtDal.InsertRestaurant(restaurant);
+            if(AffectedRow > 0 ) 
+            { return true; }
+            else 
+            { return false; }
+        }
+        public static IEnumerable<DataRow> ReadRestaurantList()
+        {
+            DataTable restaurantTable = RestaurantMgmtDal.SelectRestaurantList();
+            if (restaurantTable == null) { return null; }
+            return restaurantTable.AsEnumerable();
+        }
+
+        public static IEnumerable<DataRow>  ReadCuisineList()
+        {
+            
+            DataTable cuisineTable = RestaurantMgmtDal.SelectCuisineList();
+            if (cuisineTable == null ) { return null; }
+            return cuisineTable.AsEnumerable();
+
+
+        }
+        public static IEnumerable<DataRow>SearchRestaurant(int cuisine)
+        {
+          DataTable restaurantTable= RestaurantMgmtDal.SelectRestaurantsByCuisine(cuisine);
+
+            return restaurantTable.AsEnumerable();
+        }
+    }
+}
---
+++ RestaurantManagementSystem.DomainEntities/CuisineType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.DomainEntities
+{
+    public class CuisineType
+    {
+
+        public int Id { get; set; }
+        public string Cusine { get; set; }
+
+    }
+}
